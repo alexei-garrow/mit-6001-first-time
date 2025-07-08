0722ddc553ce91a90074e6b12be24d344dcc544b
Alexei Garrow <alexei.garrow@gmail.com>
2025-07-08 16:06:12 +1200

    not so simple..

---
+++ problem-two.py
@@ -0,0 +1,18 @@
+# Assume s is a string of lower case characters.
+
+# Write a program that prints the number of times the string 'bob' occurs in s. 
+# For example, if s = 'azcbobobegghakl', then your program should print
+
+# Number of times bob occurs is: 2
+
+s = 'azcbobobegghakl'
+bob = 'bob'
+
+def bob_count(s):
+    count = 0
+    for value in s:
+        if value bob:
+            count += 1
+    return count
+
+print("Number of vtimes bob occurs is:", bob_count(s))
