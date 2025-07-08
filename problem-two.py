9a3ca71b2fcd4fe7142fd3489b8efa2d892794e2
Alexei Garrow <alexei.garrow@gmail.com>
2025-07-08 17:22:23 +1200

    workable solutions with help

--- problem-two.py
+++ problem-two.py
@@ -5,24 +5,50 @@
 
 # Number of times bob occurs is: 2
 
-s = 'azcbobobegghakl'
-bob = 'bob'
+s: str = 'azcbobobegghakl'
+bob: str = 'bob'
 
+# Grabs a count of 5 because it checks each character of bob being 'b' and 'o' 
+# def bob_count(s):
+#     count = 0
+#     for value in s:
+#         if value in bob:
+#             count += 1
+#     return count
 
-def bob_count(s):
+# print("Number of times bob occurs is:", bob_count(s))
+
+# GitHub copilot, gave me the direct answer when I asked why bob was underlined...:
+# I got it to add the comments as I wanted to understand the answer
+
+# def bob_count(s: str):
+#     count: int = 0
+#     for i in range(len(s) - 2):  # stop 2 characters before the end
+#         if s[i:i+3] == 'bob':    # check 3-character slices
+#             count += 1
+#     return count
+
+# print("Number of times bob occurs is:", bob_count(s))
+
+# an expansion that shows progress 
+
+def bob_count(s: str) -> int:
     count = 0
-    for value in s:
-        if value in bob:
+    for i in range(len(s) - 2):
+        slice = s[i:i+3]
+        print(f"Checking slice: {slice}")
+        if slice == 'bob':
             count += 1
     return count
 
+s = 'azcbobobegghakl'
 print("Number of times bob occurs is:", bob_count(s))
 
-# GitHub copilot, gave me the direct answer when I asked why bob was udnerlined...
 
-# My own go at a solution from some stack overflow thinking
+
+# My own brainstorming at a solution from some stack overflow thinking
 
 # def bob_counts(s):
-#     words = s.split("   ")
+#     words = s.split("   ") # would only get spaces, and there aren't that many
 #     print(words)
 
